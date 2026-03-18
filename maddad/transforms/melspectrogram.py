ee4b9944cf38fb840a4840305256f13fa9156fff
tky823 <tky823.main+github@gmail.com>
2026-03-19 00:28:35 +0900

    [src] Add MelSpectrogram for BeatThis

---
+++ maddad/transforms/melspectrogram.py
@@ -0,0 +1,54 @@
+from typing import Callable, Dict, Optional
+
+import torch
+import torchaudio.transforms as aT
+
+
+class BeatThisMelSpectrogram(aT.MelSpectrogram):
+    def __init__(
+        self,
+        sample_rate: int = 22050,
+        n_fft: int = 1024,
+        win_length: Optional[int] = None,
+        hop_length: int = 441,
+        f_min: float = 30,
+        f_max: Optional[float] = 11000,
+        pad: int = 0,
+        n_mels: int = 128,
+        window_fn: Callable[[int], torch.Tensor] = torch.hann_window,
+        power: float = 1.0,
+        normalized: bool = "frame_length",
+        wkwargs: Optional[Dict] = None,
+        center: bool = True,
+        pad_mode: str = "reflect",
+        onesided: Optional[bool] = None,
+        norm: Optional[str] = None,
+        mel_scale: str = "slaney",
+        scale: float = 1000,
+    ) -> None:
+        super().__init__(
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            win_length=win_length,
+            hop_length=hop_length,
+            f_min=f_min,
+            f_max=f_max,
+            pad=pad,
+            n_mels=n_mels,
+            window_fn=window_fn,
+            power=power,
+            normalized=normalized,
+            wkwargs=wkwargs,
+            center=center,
+            pad_mode=pad_mode,
+            onesided=onesided,
+            norm=norm,
+            mel_scale=mel_scale,
+        )
+
+        self.scale = scale
+
+    def forward(self, waveform: torch.Tensor) -> torch.Tensor:
+        spec = super().forward(waveform)
+
+        return torch.log1p(self.scale * spec)
