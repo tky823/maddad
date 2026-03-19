5866baa9827162ff4dac33c696f73f5dd4e6f9e3
tky823 <tky823.main+github@gmail.com>
2026-03-20 01:15:05 +0900

    [src] Support decoder by pipeline

--- maddad/pipelines/beat/beatthis.py
+++ maddad/pipelines/beat/beatthis.py
@@ -1,14 +1,17 @@
 import os
+import warnings
 from typing import Any, Dict, Optional, Union
 
 import hydra
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchaudio
 from omegaconf import OmegaConf
 
 from ...models.beatthis import BeatThis
 from ...transforms import BeatThisTransform
+from ...transforms.beatthis import MinimalBeatDecoder
 from .base import BeatPipeline
 
 
@@ -16,7 +19,7 @@ class BeatThisPipeline(BeatPipeline):
     def __init__(
         self,
         model: nn.Module,
-        postprocessing: Optional[str] = "dbn",
+        decoder: Optional[nn.Module] = None,
         chunking: bool = True,
         device: Optional[torch.device] = None,
     ) -> None:
@@ -28,17 +31,22 @@ class BeatThisPipeline(BeatPipeline):
             else:
                 device = torch.device("cpu")
 
+        if decoder is None:
+            decoder = MinimalBeatDecoder()
+
         self.transform = BeatThisTransform()
         self.model = model
-        self.postprocessing = postprocessing
+        self.decoder = decoder
         self.chunking = chunking
         self.device = device
 
         self.transform.to(self.device)
         self.model.to(self.device)
+        self.decoder.to(self.device)
 
         self.transform.eval()
         self.model.eval()
+        self.decoder.eval()
 
     def __call__(
         self, input: Union[torch.Tensor, str], sample_rate: Optional[int] = None
@@ -70,24 +78,59 @@ class BeatThisPipeline(BeatPipeline):
             )
 
         waveform = waveform.to(self.device)
-        spectrogram = self.transform(waveform)
+
+        if isinstance(self.transform, BeatThisTransform):
+            spectrogram, last_offset = self.transform(waveform)
+        else:
+            spectrogram = self.transform(waveform)
+            last_offset = None
 
         if self.chunking:
-            output = []
+            logit = []
 
             for _spectrogram in spectrogram:
-                _output, _ = self.model(_spectrogram.unsqueeze(dim=0))
-                output.append(_output)
+                output, _ = self.model(_spectrogram.unsqueeze(dim=0))
+                logit.append(output)
 
-            output = torch.cat(output, dim=0)
+            logit = torch.cat(logit, dim=0)
         else:
-            output, _ = self.model(spectrogram)
+            logit, _ = self.model(spectrogram)
+
+        if isinstance(self.transform, BeatThisTransform):
+            pad = self.transform.pad
+            logit = F.pad(logit, (-pad, -pad))
+            num_chunks = logit.size(0)
+
+            logit, last_logit = torch.split(logit, [num_chunks - 1, 1], dim=0)
+            _, last_logit = torch.split(
+                last_logit, [last_offset, last_logit.size(-1) - last_offset], dim=-1
+            )
+            logit = logit.view(1, -1)
+            logit = torch.cat([logit, last_logit], dim=-1)
+        else:
+            warnings.warn(
+                f"{type(self.transform)} is not supported, so no trimming is applied.",
+                UserWarning,
+            )
+
+        peaks = self.decoder(logit)
+        peaks = peaks.squeeze(dim=0)
+
+        frame_rate = self.transform.hop_length / self.transform.sample_rate
+
+        output = {
+            "beat": peaks * frame_rate,
+        }
 
         return output
 
     @classmethod
     def build_from_pretrained(
-        cls, checkpoint: str, *, postprocessing: str = "dbn", device: torch.device | None = None
+        cls,
+        checkpoint: str,
+        *,
+        decoder: Optional[Union[nn.Module, str]] = None,
+        device: torch.device | None = None,
     ) -> "BeatThisPipeline":
         if device is None:
             if torch.cuda.is_available():
@@ -104,8 +147,8 @@ class BeatThisPipeline(BeatPipeline):
             model_state_dict = state_dict["model"]
             model.load_state_dict(model_state_dict)
 
-            return cls(model, postprocessing=postprocessing, device=device)
+            return cls(model, decoder=decoder, device=device)
         else:
             model = BeatThis.build_from_pretrained(checkpoint)
 
-            return cls(model, postprocessing=postprocessing, device=device)
+            return cls(model, decoder=decoder, device=device)
