e42eda30f6b7aa08f1128ff85ac5043630db51dc
tky823 <tky823.main+github@gmail.com>
2026-03-19 22:10:11 +0900

    [src] Add cache directories to __all__

--- maddad/utils/__init__.py
+++ maddad/utils/__init__.py
@@ -1,4 +1,9 @@
 from .cache import get_cache_dir, get_model_cache_dir
 
-audyn_cache_dir = get_cache_dir()
+__all__ = [
+    "maddad_cache_dir",
+    "model_cache_dir",
+]
+
+maddad_cache_dir = get_cache_dir()
 model_cache_dir = get_model_cache_dir()
