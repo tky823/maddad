e55838d27fe3ee8fef614deb853ca7ef04e1a508
tky823 <tky823.main+github@gmail.com>
2026-03-19 17:54:03 +0900

    [src] Add RoFormerEncoderLayer

--- maddad/modules/beatthis.py
+++ maddad/modules/beatthis.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Callable, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
@@ -47,6 +47,153 @@ class Frontend(nn.Module):
         output = self.activation(x)
 
         return output
+
+
+class RoFormerEncoderLayer(nn.Module):
+    """Encoder layer of RoFormer for BeatThis."""
+
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+        activation: Union[str, Callable[[torch.Tensor], torch.Tensor]] = F.relu,
+        layer_norm_eps: float = 1e-5,
+        rope_base: int = 10000,
+        qdim: Optional[int] = None,
+        kdim: Optional[int] = None,
+        vdim: Optional[int] = None,
+        share_heads: bool = True,
+        batch_first: bool = False,
+        norm_first: bool = False,
+        bias: bool = True,
+        device: torch.device = None,
+        dtype: torch.dtype = None,
+    ) -> None:
+        factory_kwargs = {
+            "device": device,
+            "dtype": dtype,
+        }
+
+        super().__init__()
+
+        self.self_attn = RotaryPositionalMultiheadAttention(
+            d_model,
+            nhead,
+            dropout=dropout,
+            bias=bias,
+            qdim=qdim,
+            kdim=kdim,
+            vdim=vdim,
+            base=rope_base,
+            share_heads=share_heads,
+            batch_first=batch_first,
+            **factory_kwargs,
+        )
+
+        self.linear1 = nn.Linear(d_model, dim_feedforward, bias=True, **factory_kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model, bias=True, **factory_kwargs)
+
+        self.norm_first = norm_first
+        self.norm1 = nn.RMSNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.norm2 = nn.RMSNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        if isinstance(activation, str):
+            activation = get_activation(activation)
+
+        if activation is F.relu or isinstance(activation, nn.ReLU):
+            self.activation_relu_or_gelu = 1
+        elif activation is F.gelu or isinstance(activation, nn.GELU):
+            self.activation_relu_or_gelu = 2
+        else:
+            self.activation_relu_or_gelu = 0
+
+        self.activation = activation
+
+    def forward(
+        self,
+        src: torch.Tensor,
+        src_mask: Optional[torch.Tensor] = None,
+        src_key_padding_mask: Optional[torch.Tensor] = None,
+        is_causal: bool = False,
+    ) -> torch.Tensor:
+        r"""Pass the input through the encoder layer.
+
+        Args:
+            src (torch.Tensor): the sequence to the encoder layer.
+            src_mask (torch.BoolTensor, optional): the mask for the src sequence.
+            src_key_padding_mask (torch.BoolTensor, optional): the mask for the src keys
+                per batch.
+            is_causal: If specified, applies a causal mask as ``src mask``.
+                Default: ``False``.
+                Warning:
+                ``is_causal`` provides a hint that ``src_mask`` is the
+                causal mask. Providing incorrect hints can result in
+                incorrect execution, including forward and backward
+                compatibility.
+
+        """
+        src_key_padding_mask = F._canonical_mask(
+            mask=src_key_padding_mask,
+            mask_name="src_key_padding_mask",
+            other_type=F._none_or_dtype(src_mask),
+            other_name="src_mask",
+            target_type=src.dtype,
+        )
+
+        src_mask = F._canonical_mask(
+            mask=src_mask,
+            mask_name="src_mask",
+            other_type=None,
+            other_name="",
+            target_type=src.dtype,
+            check_other=False,
+        )
+
+        x = src
+
+        if self.norm_first:
+            x = x + self._sa_block(
+                self.norm1(x), src_mask, src_key_padding_mask, is_causal=is_causal
+            )
+            x = x + self._ff_block(self.norm2(x))
+        else:
+            x = self.norm1(
+                x + self._sa_block(x, src_mask, src_key_padding_mask, is_causal=is_causal)
+            )
+            x = self.norm2(x + self._ff_block(x))
+
+        return x
+
+    def _sa_block(
+        self,
+        x: torch.Tensor,
+        attn_mask: Optional[torch.Tensor],
+        key_padding_mask: Optional[torch.Tensor],
+        is_causal: bool = False,
+    ) -> torch.Tensor:
+        if is_causal:
+            raise NotImplementedError("is_causal=True is not supported.")
+
+        x, _ = self.self_attn(
+            x,
+            x,
+            x,
+            attn_mask=attn_mask,
+            key_padding_mask=key_padding_mask,
+            need_weights=False,
+        )
+
+        return self.dropout1(x)
+
+    def _ff_block(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+
+        return self.dropout2(x)
 
 
 class RotaryPositionalMultiheadAttention(_RotaryPositionalMultiheadAttention):
@@ -264,3 +411,23 @@ class RotaryPositionalMultiheadAttention(_RotaryPositionalMultiheadAttention):
             attn_weights = None
 
         return output, attn_weights
+
+
+def get_activation(activation: str) -> nn.Module:
+    """Get activation module by str.
+
+    Args:
+        activation (str): Name of activation module.
+
+    Returns:
+        nn.Module: Activation module.
+
+    """
+    if activation == "relu":
+        return nn.ReLU()
+    elif activation == "gelu":
+        return nn.GELU()
+    elif activation == "elu":
+        return nn.ELU()
+
+    raise RuntimeError(f"activation should be relu/gelu/elu, not {activation}")
