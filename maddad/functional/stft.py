11086c0ec2792237cae99eacb7064a0f97fe8fef
tky823 <tky823.main+github@gmail.com>
2026-03-21 12:27:56 +0900

    [src] Fix padding to be compatible with madmom

--- maddad/functional/stft.py
+++ maddad/functional/stft.py
@@ -11,6 +11,21 @@ def stft(
     window: torch.Tensor = torch.hann_window,
     include_nyquist: bool = False,
 ) -> torch.Tensor:
+    """Wrapper function of torch.stft compatible with madmom.
+
+    Args:
+        input (torch.Tensor): Waveform of shape (*, num_samples).
+        n_fft (int): FFT window size.
+        hop_length (int): Hop length.
+        window (torch.Tensor or callable, optional): torch window function (e.g., ``torch.hann_window``), \
+            or callable that takes n_fft as argument and returns window tensor.
+        include_nyquist: Whether to include Nyquist frequency bin in output.
+
+    Returns:
+        torch.Tensor: STFT of shape (*, num_bins, num_frames), where num_bins is n_fft // 2 + 1 \
+            if ``include_nyquist`` is ``True``, else n_fft // 2.
+
+    """
     if window is None:
         _window = window
     elif isinstance(window, torch.Tensor):
@@ -23,9 +38,10 @@ def stft(
         raise ValueError(f"Unsupported window type: {type(window)}")
 
     num_samples = input.size(-1)
-    num_frames = num_samples // hop_length
+    num_frames = (num_samples - 1) // hop_length + 1
+    padding = (num_frames - 1) * hop_length + n_fft - num_samples
     left_padding = n_fft // 2
-    right_padding = (num_frames - 1) * hop_length + n_fft - (num_samples + left_padding)
+    right_padding = padding - left_padding
     x = F.pad(input, (left_padding, right_padding))
 
     output = torch.stft(
