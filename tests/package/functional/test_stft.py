34a8f46a35998eee593fce67a12b7f3e152d6ea1
tky823 <tky823.main+github@gmail.com>
2026-03-21 12:28:56 +0900

    [tests] Add test of stft

---
+++ tests/package/functional/test_stft.py
@@ -0,0 +1,27 @@
+import os
+
+import torch
+from maddad_testing.utils import maddad_testing_cache_dir as cache_dir
+
+from maddad.functional import stft
+from maddad.utils._github import download_file_from_github_release
+
+
+def test_stft() -> None:
+    url = "https://github.com/tky823/maddad/releases/download/v0.0.0/test_madmom_stft.pth"
+    path = os.path.join(cache_dir, "test_madmom_stft.pth")
+    download_file_from_github_release(url, path)
+
+    data = torch.load(path)
+    waveform = data["waveform"]
+    sample_rate = data["sample_rate"]
+    expected_output = data["spectrogram"]
+
+    frame_size = 2048
+    hop_size = 441
+
+    assert sample_rate == 22050
+
+    output = stft(waveform, n_fft=frame_size, hop_length=hop_size)
+
+    assert torch.allclose(output, expected_output, atol=1e-5)
