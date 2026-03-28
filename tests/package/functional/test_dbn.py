6f893af76b7a6c9d7283a557dec14d3a799cf37b
tky823 <tky823.main+github@gmail.com>
2026-03-29 00:07:15 +0900

    [tests] Set larger value as threshold in tests

--- tests/package/functional/test_dbn.py
+++ tests/package/functional/test_dbn.py
@@ -6,12 +6,13 @@ from maddad.functional.dbn import decode_beat_peaks_by_viterbi
 def test_decode_beat_peaks_by_viterbi() -> None:
     torch.manual_seed(0)
 
-    batch_size = 2
-    num_frames = 2000
+    batch_size = 4
+    num_frames = 1000
     frame_rate = 50
+    threshold = 0.8
 
     logit = torch.randn(batch_size, num_frames)
-    peaks = decode_beat_peaks_by_viterbi(logit, frame_rate=frame_rate)
+    peaks = decode_beat_peaks_by_viterbi(logit, frame_rate=frame_rate, threshold=threshold)
 
     assert peaks.dim() == 2
     assert peaks.size(0) == batch_size
