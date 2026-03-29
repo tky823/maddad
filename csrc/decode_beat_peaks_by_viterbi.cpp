53eab68aa51e2917f1f77420c90553322602c8f3
tky823 <tky823.main+github@gmail.com>
2026-03-29 22:18:32 +0900

    [src] Take log-probability by decode_beat_peaks_by_viterbi

--- csrc/decode_beat_peaks_by_viterbi.cpp
+++ csrc/decode_beat_peaks_by_viterbi.cpp
@@ -10,13 +10,9 @@
 
 namespace
 {
-    float log_sigmoid(float input)
-    {
-        return std::min(0.0f, input) - std::log1p(std::exp(-std::abs(input)));
-    }
-
     void unbatched_decode(
-        const float *logit_ptr,
+        const float *beat_log_prob_ptr,
+        const float *nonbeat_log_prob_ptr,
         int8_t *peaks_ptr,
         int64_t num_frames,
         int64_t num_states,
@@ -28,9 +24,9 @@ namespace
         float inf = std::numeric_limits<float>::infinity();
 
         std::vector<int64_t> best_prev(num_frames * num_states, 0);
-        std::vector<float> log_prob(num_frames * num_states, -inf);
+        std::vector<float> score(num_frames * num_states, -inf);
         int64_t *best_prev_ptr = best_prev.data();
-        float *log_prob_ptr = log_prob.data();
+        float *score_ptr = score.data();
 
         // frame_index = 0: initial distribution
         for (int64_t fpb_index = 0; fpb_index < num_fpbs; fpb_index++)
@@ -39,22 +35,23 @@ namespace
             int64_t fpb = fpbs_ptr[fpb_index];
 
             // first state
-            log_prob_ptr[offset] = log_sigmoid(logit_ptr[0]);
+            score_ptr[offset] = beat_log_prob_ptr[0];
 
             // other states
             for (int64_t _state_index = 1; _state_index < fpb; _state_index++)
             {
                 int64_t state_index = offset + _state_index;
-                log_prob_ptr[state_index] = log_sigmoid(-logit_ptr[0]);
+                score_ptr[state_index] = nonbeat_log_prob_ptr[0];
             }
         }
 
         // frame_index > 0
         for (int64_t frame_index = 1; frame_index < num_frames; frame_index++)
         {
-            float _logit = logit_ptr[frame_index];
-            float *_prev_log_prob_ptr = log_prob_ptr + (frame_index - 1) * num_states;
-            float *_log_prob_ptr = log_prob_ptr + frame_index * num_states;
+            float _beat_log_prob = beat_log_prob_ptr[frame_index];
+            float _nonbeat_log_prob = nonbeat_log_prob_ptr[frame_index];
+            float *_prev_score_ptr = score_ptr + (frame_index - 1) * num_states;
+            float *_score_ptr = score_ptr + frame_index * num_states;
             int64_t *_best_prev_ptr = best_prev_ptr + frame_index * num_states;
 
             for (int64_t fpb_index = 0; fpb_index < num_fpbs; fpb_index++)
@@ -62,7 +59,7 @@ namespace
                 int64_t offset = offsets_ptr[fpb_index];
                 int64_t fpb = fpbs_ptr[fpb_index];
 
-                float best_log_prob = -inf;
+                float best_score = -inf;
                 int64_t best_prev_state = 0;
 
                 // first state: frame per beat may be changed
@@ -73,23 +70,23 @@ namespace
                     int64_t prev_state_idx = prev_offset + prev_fpb - 1;
 
                     float _log_transition_prob = log_transition_prob_ptr[prev_fpb_index * num_fpbs + fpb_index];
-                    float prob = _prev_log_prob_ptr[prev_state_idx] + _log_transition_prob;
+                    float score = _prev_score_ptr[prev_state_idx] + _log_transition_prob;
 
-                    if (prob > best_log_prob)
+                    if (score > best_score)
                     {
-                        best_log_prob = prob;
+                        best_score = score;
                         best_prev_state = prev_state_idx;
                     }
                 }
 
-                _log_prob_ptr[offset] = best_log_prob + log_sigmoid(_logit);
+                _score_ptr[offset] = best_score + _beat_log_prob;
                 _best_prev_ptr[offset] = best_prev_state;
 
                 // other states: frame per beat is not changed
                 for (int64_t state_index = 1; state_index < fpb; state_index++)
                 {
                     int64_t _state_index = offset + state_index;
-                    _log_prob_ptr[_state_index] = _prev_log_prob_ptr[_state_index - 1] + log_sigmoid(-_logit);
+                    _score_ptr[_state_index] = _prev_score_ptr[_state_index - 1] + _nonbeat_log_prob;
                     _best_prev_ptr[_state_index] = _state_index - 1;
                 }
             }
@@ -98,13 +95,13 @@ namespace
         // back track
         int64_t best_state_index = 0;
         float best_final_prob = -inf;
-        float *_last_log_prob_ptr = log_prob_ptr + (num_frames - 1) * num_states;
+        float *_last_score_ptr = score_ptr + (num_frames - 1) * num_states;
 
         for (int64_t state_index = 0; state_index < num_states; state_index++)
         {
-            if (_last_log_prob_ptr[state_index] > best_final_prob)
+            if (_last_score_ptr[state_index] > best_final_prob)
             {
-                best_final_prob = _last_log_prob_ptr[state_index];
+                best_final_prob = _last_score_ptr[state_index];
                 best_state_index = state_index;
             }
         }
@@ -135,29 +132,33 @@ namespace
 namespace maddad
 {
     at::Tensor decode_beat_peaks_by_viterbi(
-        const at::Tensor &logit, const at::Tensor &lengths, const at::Tensor &fpbs, const at::Tensor &log_transition_prob)
+        const at::Tensor &beat_log_prob, const at::Tensor &nonbeat_log_prob, const at::Tensor &lengths, const at::Tensor &fpbs, const at::Tensor &log_transition_prob)
     {
-        TORCH_CHECK(logit.dim() == 2, "logit should be 2 dim.");
+        TORCH_CHECK(beat_log_prob.dim() == 2, "beat_log_prob should be 2 dim.");
+        TORCH_CHECK(nonbeat_log_prob.dim() == 2, "nonbeat_log_prob should be 2 dim.");
+        TORCH_CHECK(nonbeat_log_prob.size(0) == beat_log_prob.size(0), "Shape of nonbeat_log_prob should be same as beat_log_prob.");
+        TORCH_CHECK(nonbeat_log_prob.size(1) == beat_log_prob.size(1), "Shape of nonbeat_log_prob should be same as beat_log_prob.");
         TORCH_CHECK(lengths.dim() == 1, "lengths should be 1 dim.");
-        TORCH_CHECK(lengths.size(0) == logit.size(0), "Batch size of lengths should be same as logit.");
+        TORCH_CHECK(lengths.size(0) == beat_log_prob.size(0), "Batch size of lengths should be same as beat_log_prob.");
         TORCH_CHECK(fpbs.dim() == 1, "fpbs should be 1 dim.");
         TORCH_CHECK(log_transition_prob.dim() == 2, "log_transition_prob should be 2 dim.");
 
-        TORCH_CHECK(logit.is_contiguous(), "logit must be contiguous.");
+        TORCH_CHECK(beat_log_prob.is_contiguous(), "beat_log_prob must be contiguous.");
+        TORCH_CHECK(nonbeat_log_prob.is_contiguous(), "beat_log_prob must be contiguous.");
         TORCH_CHECK(lengths.is_contiguous(), "lengths must be contiguous.");
         TORCH_CHECK(fpbs.is_contiguous(), "fpbs must be contiguous.");
         TORCH_CHECK(log_transition_prob.is_contiguous(), "log_transition_prob must be contiguous.");
 
-        int64_t batch_size = logit.size(0);
-        int64_t num_frames = logit.size(1);
+        int64_t batch_size = beat_log_prob.size(0);
+        int64_t num_frames = beat_log_prob.size(1);
         int64_t num_fpbs = fpbs.size(0);
 
         int64_t num_threads = torch::get_num_threads();
         int64_t grain_size = std::ceil((batch_size - 1.0) / num_threads) + 1;
 
-        torch::TensorOptions int8options = torch::TensorOptions().dtype(torch::kInt8).device(logit.device());
-        torch::TensorOptions int64options = torch::TensorOptions().dtype(torch::kInt64).device(logit.device());
-        torch::TensorOptions float32options = torch::TensorOptions().dtype(torch::kFloat32).device(logit.device());
+        torch::TensorOptions int8options = torch::TensorOptions().dtype(torch::kInt8).device(beat_log_prob.device());
+        torch::TensorOptions int64options = torch::TensorOptions().dtype(torch::kInt64).device(beat_log_prob.device());
+        torch::TensorOptions float32options = torch::TensorOptions().dtype(torch::kFloat32).device(beat_log_prob.device());
 
         int64_t *fpbs_ptr = fpbs.data_ptr<int64_t>();
 
@@ -176,7 +177,8 @@ namespace maddad
 
         at::Tensor peaks = torch::full({batch_size, num_frames}, 0, int8options);
 
-        float *logit_ptr = logit.data_ptr<float>();
+        float *beat_log_prob_ptr = beat_log_prob.data_ptr<float>();
+        float *nonbeat_log_prob_ptr = nonbeat_log_prob.data_ptr<float>();
         int64_t *lengths_ptr = lengths.data_ptr<int64_t>();
         int8_t *peaks_ptr = peaks.data_ptr<int8_t>();
 
@@ -186,12 +188,14 @@ namespace maddad
             {
                 for (int64_t batch_idx = start; batch_idx < end; batch_idx++)
                 {
-                    float *_logit_ptr = logit_ptr + batch_idx * num_frames;
+                    float *_beat_log_prob_ptr = beat_log_prob_ptr + batch_idx * num_frames;
+                    float *_nonbeat_log_prob_ptr = nonbeat_log_prob_ptr + batch_idx * num_frames;
                     int64_t _num_frames = lengths_ptr[batch_idx]; // actual number of frames for sample
                     int8_t *_peaks_ptr = peaks_ptr + batch_idx * num_frames;
 
                     unbatched_decode(
-                        _logit_ptr,
+                        _beat_log_prob_ptr,
+                        _nonbeat_log_prob_ptr,
                         _peaks_ptr,
                         _num_frames,
                         num_states,
@@ -210,7 +214,7 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
 
 TORCH_LIBRARY_FRAGMENT(maddad, m)
 {
-    m.def("decode_beat_peaks_by_viterbi(Tensor logit, Tensor lengths, Tensor fpbs, Tensor log_transition_prob) -> Tensor");
+    m.def("decode_beat_peaks_by_viterbi(Tensor beat_log_prob, Tensor nonbeat_log_prob, Tensor lengths, Tensor fpbs, Tensor log_transition_prob) -> Tensor");
 }
 
 TORCH_LIBRARY_IMPL(maddad, CPU, m)
--- maddad/functional/dbn.py
+++ maddad/functional/dbn.py
@@ -83,7 +83,8 @@ def decode_beat_peaks_by_viterbi(
         offsets = torch.argmax(non_padding_mask.long(), dim=-1)
         trimmings = torch.argmax(reversed_non_padding_mask.long(), dim=-1)
 
-        trimmed_logit = []
+        trimmed_beat_log_prob = []
+        trimmed_nonbeat_log_prob = []
         lengths = []
 
         for _logit, offset, trimming in zip(logit, offsets, trimmings):
@@ -95,16 +96,23 @@ def decode_beat_peaks_by_viterbi(
                 [offset, num_frames - offset - trimming, trimming],
                 dim=-1,
             )
-            trimmed_logit.append(_logit)
+            _beat_log_prob = F.logsigmoid(_logit)
+            _nonbeat_log_prob = F.logsigmoid(-_logit)
+            trimmed_beat_log_prob.append(_beat_log_prob)
+            trimmed_nonbeat_log_prob.append(_nonbeat_log_prob)
             lengths.append(length)
 
-        logit = nn.utils.rnn.pad_sequence(
-            trimmed_logit, batch_first=True, padding_value=-float("inf")
+        beat_log_prob = nn.utils.rnn.pad_sequence(
+            trimmed_beat_log_prob, batch_first=True, padding_value=-float("inf")
+        )
+        nonbeat_log_prob = nn.utils.rnn.pad_sequence(
+            trimmed_nonbeat_log_prob, batch_first=True, padding_value=-float("inf")
         )
         lengths = torch.tensor(lengths, dtype=torch.long, device=device)
 
     peaks = _decode_beat_peaks_by_viterbi(
-        logit=logit,
+        beat_log_prob=beat_log_prob,
+        nonbeat_log_prob=nonbeat_log_prob,
         lengths=lengths,
         fpbs=fpbs,
         log_transition_prob=log_transition_prob,
@@ -125,7 +133,8 @@ def decode_beat_peaks_by_viterbi(
 
 @torch.no_grad()
 def _decode_beat_peaks_by_viterbi(
-    logit: torch.Tensor,
+    beat_log_prob: torch.Tensor,
+    nonbeat_log_prob: torch.Tensor,
     *,
     lengths: Optional[torch.Tensor] = None,
     fpbs: Optional[torch.LongTensor] = None,
@@ -134,7 +143,8 @@ def _decode_beat_peaks_by_viterbi(
     """Search for best beat path through the state space using a dynamic Bayesian network (DBN) approach.
 
     Args:
-        logit (torch.Tensor): Logit of shape (batch_size, num_frames).
+        beat_log_prob (torch.Tensor): Log probability for beat of shape (batch_size, num_frames).
+        nonbeat_log_prob (torch.Tensor): Log probability for non-beat of shape (batch_size, num_frames).
         lengths (torch.Tensor, optional): Lengths of each sample in the batch. If not provided, \
             it is assumed that all samples have the same length. Defaults to ``None``.
         fpbs (torch.Tensor): Frames per beat to assume.
@@ -146,7 +156,7 @@ def _decode_beat_peaks_by_viterbi(
 
     """
     if lengths is None:
-        batch_size, num_frames = logit.size()
+        batch_size, num_frames = beat_log_prob.size()
         lengths = torch.full((batch_size,), num_frames, dtype=torch.long)
 
     if fpbs is None:
@@ -158,7 +168,7 @@ def _decode_beat_peaks_by_viterbi(
         log_transition_prob = F.log_softmax(-weight * ratio, dim=-1)
 
     peaks = torch.ops.maddad.decode_beat_peaks_by_viterbi.default(
-        logit, lengths, fpbs, log_transition_prob
+        beat_log_prob, nonbeat_log_prob, lengths, fpbs, log_transition_prob
     )
 
     return peaks
