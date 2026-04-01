34bdcc65411bc8aa73b45f1f36fa7274c7524e70
tky823 <tky823.main+github@gmail.com>
2026-04-02 00:32:51 +0900

    [tests] Add test of decode_beat_and_downbeat_peaks_by_viterbi

--- tests/package/functional/test_dbn.py
+++ tests/package/functional/test_dbn.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from maddad.functional.dbn import (
+    decode_beat_and_downbeat_peaks_by_viterbi,
     decode_beat_peaks_by_viterbi,
 )
 
@@ -44,3 +45,31 @@ def test_decode_beat_peaks_by_viterbi() -> None:
 
     assert unbatched_peaks.size() == peaks.size()
     assert torch.equal(unbatched_peaks, peaks)
+
+
+def test_decode_beat_and_downbeat_peaks_by_viterbi() -> None:
+    torch.manual_seed(0)
+
+    batch_size = 4
+    num_frames = 1000
+    frame_rate = 50
+    threshold = 0.8
+
+    beat_logit = torch.randn(batch_size, num_frames)
+    downbeat_logit = torch.randn(batch_size, num_frames)
+    beat_log_prob = F.logsigmoid(beat_logit)
+    downbeat_log_prob = F.logsigmoid(downbeat_logit)
+    nonbeat_log_prob = -torch.max(beat_log_prob, downbeat_log_prob)
+
+    peaks, beats = decode_beat_and_downbeat_peaks_by_viterbi(
+        beat_log_prob,
+        downbeat_log_prob,
+        nonbeat_log_prob,
+        frame_rate=frame_rate,
+        threshold=threshold,
+    )
+
+    assert peaks.dim() == 2
+    assert peaks.size(0) == batch_size
+    assert beats.dim() == 2
+    assert beats.size(0) == batch_size
