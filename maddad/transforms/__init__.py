e207b7fd6014e084e297016a1d02f0ad130587ac
tky823 <tky823.main+github@gmail.com>
2026-04-05 17:07:10 +0900

    [src] Add DBNBeatAndDownbeatDecoder to __all__

--- maddad/transforms/__init__.py
+++ maddad/transforms/__init__.py
@@ -1,4 +1,5 @@
 from .beat import DBNBeatDecoder
+from .beat_and_downbeat import DBNBeatAndDownbeatDecoder
 from .beatthis import BeatThisMelSpectrogram, BeatThisTransform
 from .stft import STFT, ShortTimeFourierTransform
 
@@ -8,4 +9,5 @@ __all__ = [
     "BeatThisMelSpectrogram",
     "BeatThisTransform",
     "DBNBeatDecoder",
+    "DBNBeatAndDownbeatDecoder",
 ]
